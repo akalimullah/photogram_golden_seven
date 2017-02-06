6cd7935d9dc4962b7ecbbc840f9e6ffa5618937d
akalimullah <akalimullah@users.noreply.github.com>
2017-02-05 20:01:24 -0600

    Finished project

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -19,6 +19,18 @@ class PhotosController < ApplicationController
   def destroy
     the_photo = Photo.find(params[:id])
     the_photo.destroy
+    redirect_to("http://localhost:3000/photos")
   end
 
+  def edit_form
+    @the_photo = Photo.find(params[:id])
+  end
+
+  def update_row
+    the_photo = Photo.find(params[:id])
+    the_photo.caption = params[:the_caption]
+    the_photo.source = params[:the_source]
+    the_photo.save
+    redirect_to("http://localhost:3000/photos/" + params[:id])
+  end
 end

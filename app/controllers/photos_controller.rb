0a3bd270908522790d4e0752b282f5d48473a398
akalimullah <akalimullah@users.noreply.github.com>
2017-02-05 19:49:58 -0600

    Completed up to destroy command

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,23 @@ class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
   end
+  def show
+    @the_photo = Photo.find(params[:id])
+  end
 
+  def new_form
+  end
+  def create_row
+    the_photo = Photo.new
+    the_photo.caption = params[:the_caption]
+    the_photo.source = params[:the_source]
+    the_photo.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    the_photo = Photo.find(params[:id])
+    the_photo.destroy
+  end
 
 end
